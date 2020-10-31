a0cdf2b1d5877fba8a1d0f2711785f0b1c756121
may arden <mayard3n@gmail.com>
2020-10-30 20:46:38 -0700

    commit tree 929d43ef1619a56d388dc8eeb977e43fc5c9ef84

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+# defines the class Dog
+class Dog 
+
+# writes the name of the dog to an instance variable @name 
+    def name=(name)
+        @name = name
+    end 
+
+# writes the breed of the dog to an instance variable @breed
+    def breed=(breed)
+        @breed = breed
+    end
+
+
+# reads the name of the dog from an instance variable @name
+    def name
+        "#{@name}"
+    end 
+
+# writes the name of the dog from an instance variable @breed
+    def breed
+        "#{@breed}"
+    end
+
+end
+
+
+
+
+
+
